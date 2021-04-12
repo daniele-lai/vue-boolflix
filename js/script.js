e538d14eb84b7186ea59098fb7c4c4c77d738704
Daniele <danielelai86@gmail.com>
2021-04-12 17:16:10 +0200

    Aggiunta funzione di ricerca serie tv

--- js/script.js
+++ js/script.js
@@ -2,9 +2,11 @@ var app = new Vue ({
   el: "#app",
   data: {
     movies: [],
+    series: [],
     querySearch: ""
   },
   methods: {
+    //Funzione di ricerca film
     searchMovie: function() {
       axios.get("https://api.themoviedb.org/3/search/movie", {
         params: {
@@ -15,6 +17,20 @@ var app = new Vue ({
       })
       .then((response) => {
         this.movies = response.data.results;
+        this.querySearch = "";
+      })
+    },
+    //Funzione di ricerca serie tv
+    searchSeries: function() {
+      axios.get("https://api.themoviedb.org/3/search/tv", {
+        params: {
+          api_key: "534c5b1725a1e639774c1339a24d9b79",
+          query: this.querySearch,
+          language: "it-IT"
+        }
+      })
+      .then((response) => {
+        this.series = response.data.results;
         this.querySearch = "";
       })
     }
